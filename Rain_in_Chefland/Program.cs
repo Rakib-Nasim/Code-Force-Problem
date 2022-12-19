594e3c16ab711f5df0c203fdce5f05eaf8818a7c
Rakibul Hasan Nasim <hasan@vonome.com>
2022-12-20 00:43:40 +0600

    Rain_in_Chefland

---
+++ Rain_in_Chefland/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Rain_in_Chefland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < x; i++)
+            {
+                int y = Convert.ToInt32(Console.ReadLine());
+                if (y < 3)
+                {
+                    Console.WriteLine("LIGHT");
+                }
+                else if(y>=3 && y < 7)
+                {
+                    Console.WriteLine("MODERATE");
+                }
+                else
+                {
+                    Console.WriteLine("HEAVY");
+                }
+            }
+        }
+    }
+}
