1b51a4ad0427d676eac8842701e25ddcf52c3297
Rakibul Hasan Nasim <76495731+Rakibul7828@users.noreply.github.com>
2023-01-03 18:41:03 +0600

    Name change

---
+++ PracticeLink/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeLink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
